31907580af27713710b411ed2e9a29903ecfa99b
Ming <cwang@logitech.com>
2025-08-19 16:01:47 +0800

    add lanuch animation

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,9 @@
  *
  */
 #include "factory_test/factory_test.h"
-
+#include "view/gantom_boot_splash.h"
 static FactoryTest ft;
-
+static GantomBootSplash* _bootSplash1 = nullptr;
 void programmer_view_create(FactoryTest* ft);
 void programmer_view_update();
 void init_nvs_settings(void);
@@ -20,6 +20,9 @@ void setup()
     ft.init();
     pinMode(GPIO_NUM_15, OUTPUT); // motor
     pinMode(GPIO_NUM_13, OUTPUT); // motor
+    // Create boot splash and show it
+    _bootSplash1 = new GantomBootSplash(&ft);
+    _bootSplash1->show(3000); // Show for 2 seconds or until interaction
     programmer_view_create(&ft);
     init_nvs_settings();
 }
