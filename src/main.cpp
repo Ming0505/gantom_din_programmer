af31a068bd94e7685d7303d47d93d5f018d40784
Ming <cwang@logitech.com>
2025-09-28 12:17:46 +0800

    update to ver1.3.1

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,8 @@ static GantomBootSplash* _bootSplash1 = nullptr;
 void programmer_view_create(FactoryTest* ft);
 void programmer_view_update();
 void init_nvs_settings(void);
-extern int _app_render_props_list_size;
+void init_as_normal_mode();
+void init_as_factory_mode();
 void setup()
 {
     pinMode(GPIO_NUM_1, INPUT_PULLUP); // motor
@@ -26,9 +27,9 @@ void setup()
     _bootSplash1 = new GantomBootSplash(&ft);
     _bootSplash1->show(3000); // Show for 3 seconds or until interaction
     if(digitalRead(GPIO_NUM_1)){
-        _app_render_props_list_size = 5;
+        init_as_normal_mode();
     }else{
-        _app_render_props_list_size = 10;
+        init_as_factory_mode();
     }
     programmer_view_create(&ft);
     init_nvs_settings();
